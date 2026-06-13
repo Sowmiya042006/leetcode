a219008ce0a2830f6c194e53e630820db2fd1c98
Sowmiya042006 <sowmiya04032006@gmail.com>
2026-06-13 11:56:15 +0530

    Time: 1557 ms (9.33%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,5 +1,5 @@
 # Write your MySQL query statement below
 select product_name,year,price
 from Sales s
-natural join Product p;
-/*on s.product_id=p.product_id*/;
+left join Product p
+on s.product_id=p.product_id;
