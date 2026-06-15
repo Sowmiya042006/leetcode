08b602e3e7d87bb088da8051055eee019dc1bc57
Sowmiya042006 <sowmiya04032006@gmail.com>
2026-06-15 16:26:22 +0530

    Time: 1269 ms (29.54%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1174-sales-analysis-iii/sales-analysis-iii.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+select distinct p.product_id as product_id,p.product_name as product_name
+from Product p
+join Sales s
+on p.product_id=s.product_id 
+where sale_date between '2019-01-01' and '2019-03-31'
+and p.product_id not in(
+    select product_id
+    from Sales
+    where sale_date not between '2019-01-01' and '2019-03-31'
+);
+
