0aba95649e33a3f2f7be34bebac45c23838acb6f
Sowmiya042006 <sowmiya04032006@gmail.com>
2026-06-18 17:48:31 +0530

    Time: 779 ms (29.47%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1155-product-sales-analysis-iii/product-sales-analysis-iii.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select product_id,year as first_year,quantity,price
+from Sales
+where (product_id,year) in
+(
+    select s.product_id,min(s.year)
+    from Sales s
+    group by s.product_id
+);
